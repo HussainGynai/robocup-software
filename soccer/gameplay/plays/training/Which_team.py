4f80a56f178e4dad89d3a1df40c7d7ff41501ff3
Hussain <hgynai@gatech.edu>
2019-09-03 18:46:22 -0400

    In progress

---
+++ soccer/gameplay/plays/training/Which_team.py
@@ -0,0 +1,37 @@
+# import robocup
+# import constants
+# import play
+# import enum
+# import behavior
+# import main
+
+
+# Maintains the state of the ball's position by keeping track of which
+# half the ball is on and prints on both entering a given state and
+# continously during the execution of a given state.
+# class WhichTeam(play.Play):
+#     class State(enum.Enum):
+
+# def which_team():
+#     close = None
+#     for i in robofleet:
+#         dis = 10000
+#         if robo.pos - ball.pos() < dis:
+#             dis = robo.pos - ball.pos()
+#             close = i
+
+#     if close in our_team:
+#         team = "ours"
+#     elif close in their_team:
+#         team = "them"
+#     else:
+#         team = "no one"
+#     while team == "ours" or team == "them":
+#         time += 1
+#         print((team, time))
+#     while team == "none":
+#         if robot in ball.direction:
+#             if robot in our_team
+
+
+
